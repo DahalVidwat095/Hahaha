1e63ad00b0a81f1e7d232f98ca91374a54ad04c2
Vidwat <dahalvidwat403@gmail.com>
2021-02-16 11:52:09 +0545

    Slight change by assigning variable a .

---
+++ abs.py
@@ -0,0 +1,6 @@
+#program to find area of rectangle
+length=int(input("enter the length:"))
+breadth=int(input("enter the breadth:"))
+Area=length*breadth
+print(f"The area of rectangle is {Area}")
+a=10
